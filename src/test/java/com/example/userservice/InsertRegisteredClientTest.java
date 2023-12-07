88051b85827bf743854a5ae04a89bc5b56d6db82
newPURU <purushattom.personal@gmail.com>
2023-12-07 19:50:51 +0530

    Implemented Spring Security deployed in AWS

--- src/test/java/com/example/userservice/InsertRegisteredClientTest.java
+++ src/test/java/com/example/userservice/InsertRegisteredClientTest.java
@@ -1,36 +1,36 @@
-package com.example.userservice;
-
-
-import com.example.userservice.security.repositories.JpaRegisteredClientRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@SpringBootTest
-public class InsertRegisteredClientTest {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
-
-    @Test
-   // @Commit
-    public void insertNewClientToDb() {
-//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//                .clientId("client1")
-//                .clientSecret(passwordEncoder.encode("pass"))
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//                .redirectUri("https://oauth.pstmn.io/v1/callback")
-//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-//                .scope(OidcScopes.OPENID)
-//                .scope(OidcScopes.PROFILE)
-//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-//                .build();
-
-      //  jpaRegisteredClientRepository.save(oidcClient);
-    }
-}
+//package com.example.userservice;
+//
+//
+//import com.example.userservice.security.repositories.JpaRegisteredClientRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@SpringBootTest
+//public class InsertRegisteredClientTest {
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+//
+//    @Test
+//   // @Commit
+//    public void insertNewClientToDb() {
+////        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+////                .clientId("client1")
+////                .clientSecret(passwordEncoder.encode("pass"))
+////                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+////                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+////                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+////                .redirectUri("https://oauth.pstmn.io/v1/callback")
+////                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+////                .scope(OidcScopes.OPENID)
+////                .scope(OidcScopes.PROFILE)
+////                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+////                .build();
+////
+////        jpaRegisteredClientRepository.save(oidcClient);
+//    }
+//}
